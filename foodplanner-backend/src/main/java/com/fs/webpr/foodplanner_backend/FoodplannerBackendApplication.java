e5c2e0dd294aa1f4e1e1f851d766c0f0b94216fa
vyperrrr <gpatrik101@gmail.com>
2025-02-25 21:33:50 +0100

    init

---
+++ foodplanner-backend/src/main/java/com/fs/webpr/foodplanner_backend/FoodplannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fs.webpr.foodplanner_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodplannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodplannerBackendApplication.class, args);
+	}
+
+}
---
+++ foodplanner-backend/src/test/java/com/fs/webpr/foodplanner_backend/FoodplannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs.webpr.foodplanner_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodplannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
