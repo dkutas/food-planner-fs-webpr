ceea7a5b433ecf599410972f36288e9b60caf169
vyperrrr <gpatrik101@gmail.com>
2025-03-01 23:41:36 +0100

    added models and init migration

---
+++ foodplanner-backend/src/main/java/com/fs/webpr/foodplanner_backend/entity/model/Ingredient.java
@@ -0,0 +1,32 @@
+package com.fs.webpr.foodplanner_backend.entity.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "ingredient_category_id", nullable = false)
+    private IngredientCategory category;
+
+    @ManyToMany(mappedBy = "ingredients")
+    @ToString.Exclude
+    private Set<Recipe> recipes;
+}
