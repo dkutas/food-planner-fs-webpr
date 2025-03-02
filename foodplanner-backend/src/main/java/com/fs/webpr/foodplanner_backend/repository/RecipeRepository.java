14fff7df3fad77b9d33c37d236a1848f5ff300fd
vyperrrr <gpatrik101@gmail.com>
2025-03-02 12:46:40 +0100

    add missing repository annotations

--- foodplanner-backend/src/main/java/com/fs/webpr/foodplanner_backend/repository/RecipeRepository.java
+++ foodplanner-backend/src/main/java/com/fs/webpr/foodplanner_backend/repository/RecipeRepository.java
@@ -2,8 +2,10 @@ package com.fs.webpr.foodplanner_backend.repository;
 
 import com.fs.webpr.foodplanner_backend.entity.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface RecipeRepository extends JpaRepository<Recipe, UUID> {
 }
--- foodplanner-backend/src/main/java/com/fs/webpr/foodplanner_backend/repository/ShoppingListRepository.java
+++ foodplanner-backend/src/main/java/com/fs/webpr/foodplanner_backend/repository/ShoppingListRepository.java
@@ -2,8 +2,10 @@ package com.fs.webpr.foodplanner_backend.repository;
 
 import com.fs.webpr.foodplanner_backend.entity.model.ShoppingList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ShoppingListRepository extends JpaRepository<ShoppingList, UUID> {
 }
