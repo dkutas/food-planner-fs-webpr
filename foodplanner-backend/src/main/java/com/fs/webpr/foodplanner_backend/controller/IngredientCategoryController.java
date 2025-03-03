ca4aee24045862c6e4d3580e654a6c87cd3a6efe
vyperrrr <gpatrik101@gmail.com>
2025-03-03 21:52:16 +0100

    add ingredient category services

--- foodplanner-backend/src/main/java/com/fs/webpr/foodplanner_backend/controller/IngredientCategoryController.java
+++ foodplanner-backend/src/main/java/com/fs/webpr/foodplanner_backend/controller/IngredientCategoryController.java
@@ -1,10 +1,14 @@
 package com.fs.webpr.foodplanner_backend.controller;
 
+import com.fs.webpr.foodplanner_backend.entity.model.IngredientCategoryDAO;
 import com.fs.webpr.foodplanner_backend.service.IngredientCategoryService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ingredient/category")
@@ -13,4 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class IngredientCategoryController {
 
     private final IngredientCategoryService ingredientCategoryService;
+
+    @GetMapping
+    public List<IngredientCategoryDAO> getAll() {
+        return ingredientCategoryService.getAll();
+    }
 }
---
+++ foodplanner-backend/src/main/java/com/fs/webpr/foodplanner_backend/entity/model/IngredientCategoryDAO.java
@@ -0,0 +1,11 @@
+package com.fs.webpr.foodplanner_backend.entity.model;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class IngredientCategoryDAO {
+    private UUID id;
+    private String name;
+}
