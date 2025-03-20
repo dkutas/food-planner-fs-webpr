001bc2b759f23bc02c8ce1d2b0efee7a8f8ffd49
Kutas Domonkos <144679719+dkutas@users.noreply.github.com>
2025-03-20 19:46:57 +0100

    add fe skeleton (#3)
    Co-authored-by: domkts <kutas.d@gmail.com>

---
+++ foodplanner-frontend/src/app/components/nutritions/nutritions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nutritions',
+  templateUrl: './nutritions.component.html',
+  styleUrls: ['./nutritions.component.css']
+})
+export class NutritionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
