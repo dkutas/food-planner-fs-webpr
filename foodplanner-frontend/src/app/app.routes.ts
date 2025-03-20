3f2a69a849c0347106e8d4a8b3bc207b6e461af1
domkts <kutas.d@gmail.com>
2025-03-20 19:36:38 +0100

    add fe skeleton

--- foodplanner-frontend/src/app/app.routes.ts
+++ foodplanner-frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MealplanComponent } from './components/mealplan/mealplan.component';
+import { NutritionsComponent } from './components/nutritions/nutritions.component';
+import { PantryComponent } from './components/pantry/pantry.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { title: 'Mealplan', path: 'meal-plan', component: MealplanComponent },
+  { title: 'Nutritions', path: 'nutritions', component: NutritionsComponent },
+  { title: 'Pantry', path: 'pantry', component: PantryComponent },
+  { title: 'Recipes', path: 'recipes', component: RecipesComponent },
+  {
+    title: 'Shopping List',
+    path: 'shopping-lisr',
+    component: ShoppingListComponent,
+  },
+];
